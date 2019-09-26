0680e060afba5db58931007cbf7f1c4745a880a9
Marco Andrade <julianodemello1999@gmail.com>
2019-09-25 21:21:33 -0400

    arquivo manifest

---
+++ src/sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "cache-imc-v1"
+
+const urls = [
+  '/',
+  '/index.html',
+  '/assets/css/bootstrap.min.css',
+  '/assets/images/delete.svg',
+  '/assets/js/functions.js',
+  '/manifest.json'
+]
+
+//instalando o cache
+
+self.addEventListener('install',(event)=>{
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urls))
+  )
+})
+
+//interceptando as requisições
+
+self.addEventListener('fetch',(event) =>{
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      if(response){
+        return response;
+      }
+      return fetch(event.request)
+    })
+  )
+})
